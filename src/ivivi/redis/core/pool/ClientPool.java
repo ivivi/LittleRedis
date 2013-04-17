03606c05d812fbc172ada04b89795b0782844908
ivivi <ivivi727@gmail.com>
2013-04-17 18:05:10 +0800

    ADD:add client pool feature which just contains structure now

---
+++ src/ivivi/redis/core/pool/ClientPool.java
@@ -0,0 +1,14 @@
+package ivivi.redis.core.pool;
+
+import ivivi.redis.core.client.NIOClient;
+
+import java.util.LinkedList;
+
+public class ClientPool {
+	
+	private static LinkedList<NIOClient> linkedList;
+	
+	public static void initialPool(int poolSize) {
+		
+	} 
+}
