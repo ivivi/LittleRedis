d838ad02a3e48f49239fae504f7a2857d8b31f85
ivivi <ivivi727@gmail.com>
2013-04-19 19:48:49 +0800

    ADD:add ConfigUtil for configuration

---
+++ src/ivivi/redis/core/util/ConfigUtil.java
@@ -0,0 +1,14 @@
+package ivivi.redis.core.util;
+
+public class ConfigUtil {
+	
+	public static int getIntegerConfig(String key) {
+		
+		return 0;
+	}
+	
+	public static String getConfig(String key) {
+		
+		return null;
+	}
+}
