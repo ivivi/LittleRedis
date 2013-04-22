bca2f1f1a1f7c20877951941b97e778495803255
ivivi <ivivi727@gmail.com>
2013-04-22 11:05:54 +0800

    ADD:add static statement for initialize hashmap of configs

--- src/ivivi/redis/core/util/ConfigUtil.java
+++ src/ivivi/redis/core/util/ConfigUtil.java
@@ -2,6 +2,12 @@ package ivivi.redis.core.util;
 
 public class ConfigUtil {
 	
+	private static final HashMap<String,String> configs = new HashMap<String,String>();
+	
+	static {
+		
+	}
+	
 	public static int getIntegerConfig(String key) {
 		
 		return 0;
