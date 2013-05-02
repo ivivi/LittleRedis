50f0004ecf69bfbe3fb0b63fa8434cf02a239b1e
ivivi <ivivi727@gmail.com>
2013-05-02 18:11:53 +0800

    ADD:add Message Entity for Client

---
+++ src/ivivi/redis/core/message/Message.java
@@ -0,0 +1,20 @@
+package ivivi.redis.core.message;
+
+public class Message {
+	
+	private Head head;
+	private Body body;
+	private Tail tail;
+	
+	public static final class Head {
+		
+	}
+	
+	public static final class Body {
+	
+	}
+	
+	public static final class Tail {
+		
+	}
+}
