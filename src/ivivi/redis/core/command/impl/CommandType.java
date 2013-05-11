4b2629663f730c6e2dd37f564772ea3dd2b3a7f3
ivivi <ivivi727@gmail.com>
2013-05-11 14:08:41 +0800

    MOD:divide CommandType into two basic types which included base and ext

--- src/ivivi/redis/core/command/impl/CommandType.java
+++ src/ivivi/redis/core/command/impl/CommandType.java
@@ -2,7 +2,18 @@ package ivivi.redis.core.command.impl;
 
 public enum CommandType {
 	
-	BASE((byte)1);
+	BASE_Keys((byte)1),
+	BASE_Strings((byte)2),
+	BASE_Hashes((byte)3),
+	BASE_Lists((byte)4),
+	BASE_Sets((byte)5),
+	BASE_SortedSets((byte)6),
+	
+	EXT_PubAndSub((byte)101),
+	EXT_Transcations((byte)102),
+	EXT_Scripting((byte)103),
+	EXT_Connection((byte)104),
+	EXT_Server((byte)105);
 	
 	private byte type;
 	
