0a1959449fb3d2f83e72e4089883cda01042947e
ivivi <ivivi727@gmail.com>
2013-05-16 12:23:02 +0800

    MOD:mod value of command type from Integer to 8 bits that the highest bit indicating which category it is belonged to

--- src/ivivi/redis/core/command/impl/CommandType.java
+++ src/ivivi/redis/core/command/impl/CommandType.java
@@ -2,18 +2,25 @@ package ivivi.redis.core.command.impl;
 
 public enum CommandType {
 	
-	BASE_Keys((byte)1),
-	BASE_Strings((byte)2),
-	BASE_Hashes((byte)3),
-	BASE_Lists((byte)4),
-	BASE_Sets((byte)5),
-	BASE_SortedSets((byte)6),
+	/*
+	 * byte is 8 bits
+	 * the highest bit indicating which category it's belonged to
+	 * 0 indicating BASE
+	 * 1 indicating EXT
+	 */
 	
-	EXT_PubAndSub((byte)101),
-	EXT_Transcations((byte)102),
-	EXT_Scripting((byte)103),
-	EXT_Connection((byte)104),
-	EXT_Server((byte)105);
+	BASE_Keys((byte)1),			// 0000 0001
+	BASE_Strings((byte)2),		// 0000 0010
+	BASE_Hashes((byte)4),   	// 0000 0100
+	BASE_Lists((byte)8),      	// 0000 1000
+	BASE_Sets((byte)16),	    // 0001 0000
+	BASE_SortedSets((byte)32),	// 0010 0000
+	
+	EXT_PubAndSub((byte)129),	// 1000 0001 
+	EXT_Transcations((byte)130),// 1000 0010
+	EXT_Scripting((byte)132),	// 1000 0100
+	EXT_Connection((byte)136),	// 1000 1000
+	EXT_Server((byte)144);		// 1001 0000
 	
 	private byte type;
 	
