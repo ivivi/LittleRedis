d302f8bb7f324706fcadbdba3917369e8c80f93c
ivivi <ivivi727@gmail.com>
2013-05-16 16:57:39 +0800

    ADD:add SeparatorUtil for assembling body of message

---
+++ src/ivivi/redis/core/util/SeparatorUtil.java
@@ -0,0 +1,14 @@
+package ivivi.redis.core.util;
+
+public class SeparatorUtil {
+	
+	/*
+    private static final byte SEPARATOR_MAIN = '#';
+    private static final byte SEPARATOR_SUB = '`';
+    private static final byte SEPARATOR_SUB_SON = '~';
+	*/
+	
+    public static final byte SEPARATOR_MAIN = '\b';
+    public static final byte SEPARATOR_SUB = '\f';
+    public static final byte SEPARATOR_SUB_SON = '\n';
+}
